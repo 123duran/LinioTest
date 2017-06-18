86dfea07c65f9724c47bf35122178e706cfe58d5
123Duran <bgdbruno@gmail.com>
2017-06-18 16:04:23 -0300

    Minor changes.

--- tests/CalcFormAddTest.php
+++ tests/CalcFormAddTest.php
@@ -48,5 +48,6 @@ class CalcFormAddTest extends  TestCase
         $this->assertNotEquals(0,$this->_calcForm->calcRemainder(4,3));
         $this->assertNotEquals(0,$this->_calcForm->calcRemainder(7,5));
         $this->assertNotEquals(0,$this->_calcForm->addRemainder(1,0));
+        $this->assertNotEquals(0,$this->_calcForm->addRemainder(($this->_calcForm->calcRemainder(17,5)),($this->_calcForm->calcRemainder(17,3))));
     }
 }
